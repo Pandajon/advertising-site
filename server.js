fac367c68defd4ab74ae8a854b4f9b5c6fb2d310
Bekmurod <karimberdiyevbekmurod@gmail.com>
2022-07-01 19:53:15 -0700

    server connect to frontend

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
-
+const exphbs = require('express-handlebars')
 const app = express()
 dotenv.config()
 connectDB()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.engine('.hbs', exphbs({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
 const port = process.env.PORT || 8080
 app.listen(port, ()=> {
