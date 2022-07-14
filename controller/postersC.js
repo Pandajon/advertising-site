f3389de2393956c32a14d960faa9f59e7d97baaf
Bekmurod <karimberdiyevbekmurod@gmail.com>
2022-07-14 07:40:10 -0700

    create update & delete

--- controller/postersC.js
+++ controller/postersC.js
@@ -1,5 +1,5 @@
 const Poster = require('../models/posterM');
-
+const User = require('../models/userM');
 const postersPage = async(req,res)=> {
     try {
         const posters = await Poster.find().lean();
@@ -21,6 +21,7 @@ const addPosterPage = (req,res)=> {
     });
 };
 
+
 const addPoster = async(req,res)=> {
     try {
         const poster = {
@@ -37,9 +38,58 @@ const addPoster = async(req,res)=> {
     }
 };
 
+const onePosterPage = async(req,res)=> {
+    const poster = await Poster.findById(req.params._id).lean();
+    res.render('posters/one-poster', {
+        url: process.env.URL,
+        title: poster.title,
+        poster
+    });
+};
 
+const editePosterPage = async(req,res)=> {
+try {
+    const editePoster = await Poster.findById(req.params._id).lean();
+    res.render('posters/edite-poster', {
+        url: process.env.URL,
+        title: 'edite poster page',
+        editePoster
+    });
+} catch (err) {
+    console.log(err)
+}
+};
+
+const editePoster = async(req,res)=> {
+try {
+    const editePoster = {
+        title: req.body.title,
+        amount: req.body.amount,
+        region: req.body.region,
+        desc: req.body.desc,
+        image: 'uploads/' + req.file.filename        
+    };
+    await Poster.findByIdAndUpdate(req.params._id, editePoster); 
+    res.redirect('/posters');   
+} catch (err) {
+    console.log(err)
+};
+};
+
+const deletePoster = async(req,res)=> {
+try {
+    await Poster.findByIdAndRemove(req.params._id);
+    res.redirect('/posters');    
+} catch (err) {
+    console.log(err)
+}
+}
 module.exports = {
     postersPage,
     addPosterPage,
-    addPoster
-}
+    addPoster,
+    onePosterPage,
+    editePosterPage,
+    editePoster,
+    deletePoster
+};
--- router/postersR.js
+++ router/postersR.js
@@ -3,13 +3,21 @@ const router = Router();
 const { 
     postersPage,
     addPosterPage,
-    addPoster
+    addPoster,
+    onePosterPage,
+    editePosterPage,
+    editePoster,
+    deletePoster
  } = require("../controller/postersC");
  const upload = require('../utils/fileUpload')
 
- router.get('/', postersPage);
- router.get('/add', addPosterPage);
+router.get('/', postersPage);
+router.get('/:_id', onePosterPage);
+router.get('/add', addPosterPage);
 router.post('/add', upload.single('image'), addPoster);
+router.get('/:_id/edite', editePosterPage);
+router.post('/:_id/edite', upload.single('image'), editePoster);
+router.post('/:_id/delete', deletePoster);
 
 
  module.exports = router
